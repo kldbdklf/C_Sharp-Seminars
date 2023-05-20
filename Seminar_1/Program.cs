6383c7692af214227ebfc9756e95064b11373b30
Ivan <ivanchul@yandex.ru>
2023-05-20 12:53:51 +0400

    Solved Task 2

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,15 +1,19 @@
-﻿/*Task 1 
+﻿// Сделал все четыре кода работающими одновременно
+
+/*Task 1 
 Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 
 */
 
-Console.WriteLine("Enter 2 integers: ");
-Console.Write("Number1 = ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Number2 = ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+
+/*Console.WriteLine("Task 1 : Max and min of two numbers.");
+Console.WriteLine("Enter numbers: ");
+Console.Write("Number 1 = ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Number 2 = ");
+double number2 = Convert.ToDouble(Console.ReadLine());
 if (number1 == number2)
 {
     Console.WriteLine("The numbers are equal.");
@@ -22,3 +26,35 @@ else
 {
     Console.WriteLine("Number " + number2 + " is max. Number " + number1 + " is min.");
 }
+*/
+
+/* Task 2
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 
+*/
+
+Console.WriteLine("Task 2 : Max of numbers.");
+Console.Write("Enter amount of numbers: ");
+int amountOfNumbers = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[amountOfNumbers];
+Console.WriteLine("Enter numbers: ");
+for (int i = 0; i < amountOfNumbers; i++)
+{
+    Console.Write("Number " + (i + 1) + " : ");
+    numbers[i] = Convert.ToDouble(Console.ReadLine());
+}
+double max = numbers[0];
+for (int i = 0; i < amountOfNumbers; i++)
+{
+    if (max < numbers[i])
+    {
+        max = numbers[i];
+    }
+}
+Console.WriteLine("Max of your numbers is " + max);
+
+
+
+
