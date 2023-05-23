76eb4f3f5a1b8809cf40ff77a6269b081705c08a
Ivan <ivanchul@yandex.ru>
2023-05-23 19:18:36 +0400

    Solved Task 1

--- Seminar_2/Seminar_2/Program.cs
+++ Seminar_2/Seminar_2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Task 1 
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Task 1: ");
+Console.Write("Enter your three-digit number: ");
+int inputNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("The second figure : " + (inputNumber / 10) % 10);
