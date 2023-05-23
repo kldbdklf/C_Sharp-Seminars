bf880e28ca81ba21503829e4833198b3f8623c08
Ivan <ivanchul@yandex.ru>
2023-05-23 19:59:30 +0400

    Solved Task 2

--- Seminar_2/Seminar_2/Program.cs
+++ Seminar_2/Seminar_2/Program.cs
@@ -4,7 +4,31 @@
 782 -> 8
 918 -> 1
 */
-Console.WriteLine("Task 1: ");
+/* 
+ * Console.WriteLine("Task 1 second figure of number: ");
 Console.Write("Enter your three-digit number: ");
 int inputNumber = int.Parse(Console.ReadLine());
 Console.WriteLine("The second figure : " + (inputNumber / 10) % 10);
+*/ 
+/* Task 2 :
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Task 2 third digit in number: ");
+Console.Write("Enter your number: ");
+int inputNumber2 = int.Parse(Console.ReadLine());
+if ((inputNumber2 / 10) < 10) Console.WriteLine("There is no third digit!");
+else
+{
+    int N = 1, counter = 2;
+    while (N != 0)
+    {
+        counter++;
+        N = inputNumber2 / Convert.ToInt32(Math.Pow(10, counter));
+    } 
+    Console.WriteLine("Third digit in your number is " + (inputNumber2 / Convert.ToInt32(Math.Pow(10, (counter - 3))) % 10));
+};
+
