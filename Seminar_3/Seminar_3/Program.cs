a9b4795b98ebe67379db6b39a602051a547430db
Ivan <ivanchul@yandex.ru>
2023-05-28 12:03:26 +0400

    Solved the task 3

--- Seminar_3/Seminar_3/Program.cs
+++ Seminar_3/Seminar_3/Program.cs
@@ -38,7 +38,7 @@ else Console.WriteLine("Your number isn't palindrom!");
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
-
+/*
 Console.WriteLine("Task 2 The distance between points in 3D space: ");
 Console.Write("Enter X1: ");
 double X1 = double.Parse(Console.ReadLine());
@@ -53,4 +53,35 @@ double Y2 = double.Parse(Console.ReadLine());
 Console.Write("Enter Z2: ");
 double Z2 = double.Parse(Console.ReadLine());
 double d = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2));
-Console.Write($"D = {Math.Round(d, 2)}");
+Console.Write($"The distance between points = {Math.Round(d, 2)}");
+*/
+
+
+/*
+ Task 3:
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+
+void showTable (int number)
+{
+    int numberSign = 1;
+    if (number < 0) { numberSign = -1;
+        number *= -1;
+    }
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write((Math.Pow(i, 3) * numberSign) + "\t");
+}
+}
+
+Console.WriteLine("Task 3 The table of cubes of numbers: ");
+Console.Write("Enter your integer: ");
+int inputNumber2 = int.Parse(Console.ReadLine());
+showTable(inputNumber2);
+
+
+
