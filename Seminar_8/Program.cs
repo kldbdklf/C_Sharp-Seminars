965e331601cfb1063398485e83836a6a12165767
Ivan <ivanchul@yandex.ru>
2023-06-18 18:54:59 +0400

    Solved Task 1

--- Seminar_8/Program.cs
+++ Seminar_8/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ Task 1: 
+Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+ */
+Console.WriteLine("Task 1 Sum of main diagonal: ");
+void FillIntMatrix(int[,] matrix)
+{
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(-10, 11);
+        }
+    }
+}
+void ShowIntArray (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void ShowSumDiagonal (int[,] matrix)
+{
+    int result = 0;
+    for (int i = 0, j = 0; i < matrix.GetLength(0) && j < matrix.GetLength(1); i++, j++) { 
+    result += matrix[i, j];
+    }
+    Console.WriteLine($"Sum of diagonal = {result}");
+}
+int[,] matrix1 = new int[5, 5];
+FillIntMatrix(matrix1);
+ShowIntArray(matrix1);
+ShowSumDiagonal(matrix1);
