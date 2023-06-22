73918cf87cb3a4431fe0e27cb56032bef7bd037a
Ivan <ivanchul@yandex.ru>
2023-06-22 17:57:50 +0400

    Solved Task 2

--- Seminar_9/Program.cs
+++ Seminar_9/Program.cs
@@ -4,15 +4,48 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
  */
+using System.ComponentModel.Design;
 using System.Reflection.Metadata.Ecma335;
 
 Console.WriteLine("Task 1 Natural numbers in the range from N to 1:");
-string ShowNaturalNumbers (int N)
+string GetNumbersString (int N)
 {
     string result = String.Empty;
     if (N == 1) return N.ToString();
-    return N + ", " + ShowNaturalNumbers(N - 1);
+    return N + ", " + GetNumbersString(N - 1);
 }
+/*
 Console.Write("Enter N: ");
-int N = int.Parse(Console.ReadLine());
-Console.WriteLine(ShowNaturalNumbers(N));
+int inputN1 = int.Parse(Console.ReadLine());
+Console.WriteLine(GetNumbersString(inputN1));
+Console.WriteLine();
+*/
+/*
+ Task 2:
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+Console.WriteLine("Task 2 Sum of natural numbers in the range from M to N:");
+void ShowSumRange (int M, int N)
+{
+    Console.Write($"M = {M}; N = {N} -> ");
+    if (M < N)
+    {
+        Console.WriteLine(GetSumRange(M, N));
+    }
+    else
+    {
+        Console.WriteLine(GetSumRange(N, M));
+    }
+}
+int GetSumRange (int start, int end)
+{
+    if (start == end) return end;
+    return start + GetSumRange(start + 1, end);
+}
+Console.Write("Enter M: ");
+int inputM1 = int.Parse(Console.ReadLine());
+Console.Write("Enter N: ");
+int inputN2 = int.Parse(Console.ReadLine());
+ShowSumRange(inputM1, inputN2);
